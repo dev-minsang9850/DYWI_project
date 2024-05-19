c33af097fdc201332de22aafbc2f9f6af55ec544
dev-minsang9850 <leeminsang9850@naver.com>
2024-05-19 17:56:15 +0900

    modify all

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-document.addEventListener('DOMContentLoaded', getWeather);
 
-function getWeather() {
-    const API_KEY = 'df333e8c6c05a0c2916457f70b53c04b'; // 여기에 발급받은 OpenWeatherMap API 키를 입력하세요
-    const API_URL = 'https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=' + API_KEY + '&units=metric'; // 가져올 도시 이름에 따라 수정 가능
+const links = document.querySelectorAll("a");
 
-    fetch(API_URL)
-        .then(response => response.json())
-        .then(data => {
-            const location = data.name + ', ' + data.sys.country;
-            const temperature = data.main.temp + '℃';
-            const description = data.weather[0].description;
-
-            document.getElementById('location').textContent = location;
-            document.getElementById('temperature').textContent = '온도: ' + temperature;
-            document.getElementById('description').textContent = '날씨: ' + description;
-        })
-        .catch(error => console.error('날씨 정보를 가져오는 동안 오류가 발생했습니다.', error));
-}
+links.forEach(link => {
+  link.addEventListener("click", event => {
+    event.preventDefault();
+    event.target.classList.add("active");
+    setTimeout(() => {
+      event.target.classList.remove("active");
+    }, 500);
+  });
+});
+//# sourceURL=pen.js
+    
